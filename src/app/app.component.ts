a5838b097edc09fabd55e796e29465a31d60a76e
gmourozf <gmouroz.f@gmail.com>
2022-04-13 17:02:07 +0200

    formation dyma angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bacasable';
+  public fruit = "";
+  public addFruit() {
+    console.log( this.fruit)
+
+  }
 }
